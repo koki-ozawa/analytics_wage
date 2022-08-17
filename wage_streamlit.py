6a3de90dd16b729cecc7865715c4c2e8e3e43c58
KokiOzawa <rararaface080@gmail.com>
2022-08-18 08:47:08 +0900

    third commit

--- wage_streamlit.py
+++ wage_streamlit.py
@@ -3,14 +3,43 @@ import streamlit as st
 import pydeck as pdk
 import plotly.express as px
 
+def show_heatmap():
+
+    view = pdk.ViewState(
+        longitude=139.691648,
+        latitude=35.689185,
+        zoom=4,
+        pitch=40.5
+    )
+
+    layer = pdk.Layer(
+        "HeatmapLayer",
+        data=HeatmapData,
+        opacity=0.4,
+        get_position=["lon", "lat"],
+        threshold=0.1,
+        get_weight= "weight",
+        pickable=True
+    )
+
+    layer_map = pdk.Deck(
+        layers = layer,
+        initial_view_state=view
+    )
+
+    st.pydeck_chart(layer_map)
+
 st.title('日本の賃金データのダッシュボード')
 cur_dir='./csv_data2020'
 wage_per_one = '一人当たり賃金（万円）'
 all_wage_per_one = '全国_一人当たり賃金（万円）'
 #read csv
 df_jp_ind = pd.read_csv('./csv_data2020/雇用_医療福祉_一人当たり賃金_全国_全産業.csv', encoding='shift-jis')
-df_jp_category = pd.read_csv('./csv_data2020/雇用_医療福祉_一人当たり賃金_全国_大分類.csv', encoding='shift-jis')
+df_jp_category_broad = pd.read_csv('./csv_data2020/雇用_医療福祉_一人当たり賃金_全国_大分類.csv', encoding='shift-jis')
 df_pref_ind = pd.read_csv('./csv_data2020/雇用_医療福祉_一人当たり賃金_都道府県_全産業.csv', encoding='shift-jis')
+df_jp_ind_mid = pd.read_csv('./csv_data2020/雇用_医療福祉_一人当たり賃金_全国_中分類.csv', encoding='shift-jis')
+df_jp_category_mid = pd.read_csv('./csv_data2020/雇用_医療福祉_一人当たり賃金_都道府県_中分類.csv', encoding='shift-jis')
+df_pref_ind_broad = pd.read_csv('./csv_data2020/雇用_医療福祉_一人当たり賃金_都道府県_大分類.csv', encoding='shift-jis')
 
 st.header('2020年:一人当たり平均賃金のヒートマップ')
 
@@ -23,29 +52,9 @@ tmp = df_pref_map[wage_per_one]
 df_pref_map["weight"] = (tmp - tmp.min()) / (tmp.max() - tmp.min())
 HeatmapData = df_pref_map[['lat', 'lon', 'weight']]
 
-view = pdk.ViewState(
-    longitude=139.691648,
-    latitude=35.689185,
-    zoom=4,
-    pitch=40.5
-)
-
-layer = pdk.Layer(
-    "HeatmapLayer",
-    data=HeatmapData,
-    opacity=0.4,
-    get_position=["lon", "lat"],
-    threshold=0.1,
-    get_weight= "weight",
-    pickable=True
-)
-
-layer_map = pdk.Deck(
-    layers = layer,
-    initial_view_state=view
-)
-
-st.pydeck_chart(layer_map)
+show_heatmap = st.checkbox('Show Heatmap')
+if show_heatmap == True:
+    show_heatmap()
 
 show_df = st.checkbox('Show DataFrame')
 if show_df == True:
@@ -60,13 +69,36 @@ df_pref_mean = df_pref_ind[df_pref_ind['年齢'] == '年齢計']
 #df_pref_mean
 pref_list = df_pref_mean['都道府県名'].unique()
 
-option = st.selectbox(
+'''option = st.selectbox(
     '都道府県名',
     (pref_list)
+)'''
+options = st.multiselect(
+    'Check prefecture name you want to compare',
+    pref_list,
+    default='東京都',
 )
-df_pref_mean = df_pref_mean[df_pref_mean['都道府県名'] == option]
-df_mean_line = pd.merge(df_mean, df_pref_mean, on='集計年')
-df_mean_line = df_mean_line[['集計年', '全国_一人当たり賃金（万円）', '一人当たり賃金（万円）']]
+#選択した都道府県のデータを抽出
+colname=[]
+for i,op in enumerate(options):
+    colname.append(op)
+    #op
+    if i == 0:
+        df_pref_options = df_pref_mean[df_pref_mean['都道府県名'] == op]
+        df_pref_options.rename(columns={ '一人当たり賃金（万円）':op}, inplace=True)
+        df_pref_options.set_index('集計年', inplace=True)
+        continue
+    #df_pref_options[op] = 
+    tmp = df_pref_mean[df_pref_mean['都道府県名'] == op]
+    tmp = tmp[['集計年',wage_per_one]]
+    tmp.rename(columns={ '一人当たり賃金（万円）':op}, inplace=True)
+    tmp.set_index('集計年', inplace=True)
+    df_pref_options[op] = tmp[op]
+#df_pref_options 
+df_mean_line = pd.merge(df_mean, df_pref_options, on='集計年')
+select_column = ['集計年', '全国_一人当たり賃金（万円）'] + colname
+#df_mean_line = df_mean_line[['集計年', '全国_一人当たり賃金（万円）', '一人当たり賃金（万円）']]
+df_mean_line = df_mean_line[select_column]
 df_mean_line = df_mean_line.set_index('集計年')
 #df_mean_line
 st.line_chart(df_mean_line)
@@ -95,18 +127,23 @@ st.plotly_chart(fig)
 
 st.header('産業別の平均賃金（万円）')
 
-year_list = df_jp_category['集計年'].unique()
+year_list = df_jp_category_broad['集計年'].unique()
 option_year = st.selectbox(
     '集計年',
     (year_list)
 )
+syotei='所定内給与額（万円）'
+syouyo='年間賞与その他特別給与額（万円）'
 wage_list = ['一人当たり賃金（万円）',  '所定内給与額（万円）', '年間賞与その他特別給与額（万円）']
+wage_list = [wage_per_one, syotei, syouyo]
 
 option_wage = st.selectbox(
     '賃金の種類',
-    (wage_list)
+    (wage_list),
+    key='wage'
 )
-df_mean_catg = df_jp_category[df_jp_category['集計年'] == option_year]
+df_mean_catg = df_jp_category_broad[df_jp_category_broad['集計年'] == option_year]
+#df_mean_catg 
 max = df_mean_catg[option_wage].max() + 50
 
 fig = px.bar(
@@ -121,5 +158,47 @@ fig = px.bar(
     height=500)
 
 st.plotly_chart(fig)
+
+#産業別業種別
+
+st.header('大産業別中産業別の平均賃金（万円）')
+option_wage2 = st.selectbox(
+    '賃金の種類',
+    (wage_list),
+    key='wage2'
+
+)
+l = df_jp_category_mid['産業大分類名'].unique()
+option_mid_catg = st.selectbox(
+    '大分類',
+    (l),
+    key='broad_list'
+)
+#l2 = df_jp_category_mid[df_jp_category_mid['産業大分類名'] ==option_mid_catg]['業種中分類名'].unique()
+option_year2 = st.selectbox(
+    '集計年',
+    (year_list),
+    key='year'
+)
+
+df_mean_catg = df_jp_ind_mid[df_jp_ind_mid['集計年'] == option_year2]
+df_mean_catg = df_mean_catg[df_mean_catg['産業大分類名'] == option_mid_catg]
+df_mean_catg = df_mean_catg[df_mean_catg['一人当たり賃金（万円）'] != '-']
+df_mean_catg[option_wage2] = df_mean_catg[option_wage2].astype(float)
+#df_mean_catg
+max = df_mean_catg[option_wage2].max() + 50
+fig = px.bar(
+    df_mean_catg,
+    x = option_wage2,
+    y = '業種中分類名',
+    color =  '業種中分類名',
+    animation_frame = '年齢',
+    range_x = [0,max],
+    orientation='h',#horizonal bar
+    width=1000,
+    height=800)
+
+st.plotly_chart(fig)
+
 st.text('出典：RESAS（地域経済分析システム）')
 st.text('本結果はRESAS（地域経済分析システム）を加工して作成')
